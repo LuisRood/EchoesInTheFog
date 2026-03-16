56d88517e53e67e43fc83326f80e699d0f70f00d
Luis Felipe Rodríguez Reyes <luisrood69@gmail.com>
2026-03-16 16:35:59 -0600

    PreAlpha 1.0.0 - Feature: Se agrega el manejo de Checkpoints

---
+++ src/server/CheckpointManager.server.lua
@@ -0,0 +1,37 @@
+local Players = game:GetService("Players")
+local workspace = game:GetService("Workspace")
+
+-- Buscamos la carpeta que creaste en Studio
+local checkpointsFolder = workspace:WaitForChild("Checkpoints")
+
+print("[BACKEND] Gestor de Checkpoints Iniciado")
+
+-- Iteramos sobre todos los bloques dentro de la carpeta
+for _, checkpoint in ipairs(checkpointsFolder:GetChildren()) do
+    if checkpoint:IsA("BasePart") then
+        
+        -- Creamos un Event Listener para cuando algo toque este bloque
+        checkpoint.Touched:Connect(function(hit)
+            local character = hit.Parent
+            local player = Players:GetPlayerFromCharacter(character)
+
+            -- Validamos que quien lo pisó sea un jugador real y no el monstruo
+            if player then
+                -- Calculamos la coordenada. Le sumamos 3 metros en Y (arriba) 
+                -- para asegurar que al revivir no te quedes atorada en el piso.
+                local nuevaPosicion = checkpoint.CFrame + Vector3.new(0, 3, 0)
+                
+                -- Validamos si esta coordenada ya es la guardada
+                local checkpointActual = character:GetAttribute("UltimoCheckpoint")
+                
+                -- Esto actúa como un "Debounce". Como tu personaje tiene muchos bloques 
+                -- (pies, piernas), el evento Touched se dispara varias veces seguidas.
+                -- Al validar que sea diferente, evitamos spamear la memoria.
+                if checkpointActual ~= nuevaPosicion then
+                    character:SetAttribute("UltimoCheckpoint", nuevaPosicion)
+                    print("[SISTEMA] Punto de control guardado para " .. player.Name)
+                end
+            end
+        end)
+    end
+end
